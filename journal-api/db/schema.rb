06305b4bf8d8ed638d833a623d1fc4c606ebcc38
Noah Fairbairn <noahfairbairn@gmail.com>
2019-07-26 10:00:18 -0700

    switched to pg and spit out some json

--- journal-api/db/schema.rb
+++ journal-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_25_234145) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.string "content"
