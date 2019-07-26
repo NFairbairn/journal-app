365b8022ea2d9381151d875795154dd934897f89
jenshaw86 <bloo.j.shaw@gmail.com>
2019-07-26 10:17:05 -0700

    Add controller methods

--- journal-api/app/controllers/entries_controller.rb
+++ journal-api/app/controllers/entries_controller.rb
@@ -1,2 +1,24 @@
 class EntriesController < ApplicationController
+    def index 
+        entries = Entry.all
+        render :json => entries
+    end
+
+    def show
+        entry = Entry.find(params[:id])
+        render :json => entry
+    end
+
+    def update
+        entry = Entry.find(params[:id])
+        entry.update(title: params[:title], content: params[:content])
+        render :json => entry
+    end
+
+    def destroy
+        entry = Entry.find(params[:id])
+        entries = Entry.all
+        entry.delete()
+        render :json => entries       
+    end
 end
--- journal-api/app/controllers/journals_controller.rb
+++ journal-api/app/controllers/journals_controller.rb
@@ -9,4 +9,17 @@ class JournalsController < ApplicationController
         journal = Journal.find(params[:id])
         render :json => journal
     end
+
+    def update
+        journal = Journal.find(params[:id])
+        journal.update(subject: params[:subject])
+        render :json => journal
+    end
+
+    def destroy
+        journal = Journal.find(params[:id])
+        journals = Journal.all
+        journal.delete()
+        render :json => journals       
+    end
 end
